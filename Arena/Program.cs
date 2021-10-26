63386d99da589e8cfc56f88377abe0ab336141b6
Tamás Szép <tamas.szep@griffsoft.hu>
2021-10-26 12:56:08 +0200

    Harcrendszer refaktorálása.

--- Arena/Program.cs
+++ Arena/Program.cs
@@ -1,72 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using Arena.Interfaces;
 using Arena.Model;
 
 namespace Arena
 {
     class Program
     {
+        static List<Fighter> fighters = new List<Fighter>();
+        static int actualFighter = 0;
+
         static void Main(string[] args)
         {
+            InitFighters();
+            ShowFighters();
 
-            Fighter warrior1 = new Fighter("Roland", 100, 30, 15);
-            Creature creature1 = new Creature("Bálint", 100, 30, 15, 1);
-
-            //TODO 1 öngyógyítást megvalósítani
-
-            //TODO 2 refaktorálni a harcot
-
-            Console.WriteLine($"1. harcos: {warrior1.Name}");
-            Console.WriteLine($"2. harcos: {creature1.Name}");
+            //TODO öngyógyítást megvalósítani
 
             bool isGameOver = false;
+            Fighter actualAttacker;
+            Fighter actualDefender;
+
+            // A játékmenet főciklusa.
             do
             {
-                int damage;
-                int block;
+                actualAttacker = fighters[getNextFighter(actualFighter, 0)];
+                actualDefender = fighters[getNextFighter(actualFighter, 1)];
 
-                damage = warrior1.Attack();
-                block = creature1.Block();
+                var damage = actualAttacker.Attack();
+                var block = actualDefender.Block();
+
                 if (block > damage)
                 {
                     block = damage;
                 }
 
-                creature1.Health -= damage - block;
+                actualDefender.Health -= damage - block;
 
-                Console.WriteLine($"{warrior1.Name} támad. Sebzés: {damage}.");
-                Console.WriteLine($"{creature1.Name} véd. Blokk: {block}.");
-                Console.WriteLine($"{creature1.Name} élete: {creature1.Health}");
+                //ShowActionResult()
+                Console.WriteLine($"{actualAttacker.Name} támad. Sebzés: {damage}.");
+                Console.WriteLine($"{actualDefender.Name} véd. Blokk: {block}.");
+                Console.WriteLine($"{actualDefender.Name} élete: {actualDefender.Health}");
                 Console.WriteLine();
 
-                if (creature1.Health <= 0)
-                {
-                    isGameOver = true;
-                }
-
-                damage = creature1.Attack();
-                block = warrior1.Block();
-                if (block > damage)
-                {
-                    block = damage;
-                }
-
-                warrior1.Health -= damage - block;
-
-                Console.WriteLine($"{creature1.Name} támad. Sebzés: {damage}.");
-                Console.WriteLine($"{warrior1.Name} véd. Blokk: {block}.");
-                Console.WriteLine($"{warrior1.Name} élete: {warrior1.Health}");
-                Console.WriteLine();
-
-                if (warrior1.Health <= 0)
+                if (actualDefender.Health <= 0)
                 {
                     isGameOver = true;
                 }
             } while (!isGameOver);
 
             Console.WriteLine("Játék vége");
-            string nyertes = warrior1.Health > creature1.Health ? warrior1.Name : creature1.Name;
-            Console.WriteLine($"A nyertes: {nyertes}");
+            Console.WriteLine($"A nyertes: {actualAttacker.Name}");
 
+        }
+
+        private static void ShowFighters()
+        {
+            foreach (var fighter in fighters)
+            {
+                Console.WriteLine($"Harcos: {fighter.Name}, Támadás: {fighter.AttackMax}, Védés: {fighter.BlockMax}");
+            }
+        }
+
+        private static void InitFighters()
+        {
+            fighters.Add(new Fighter("Roland", 100, 30, 15));
+            fighters.Add(new Creature("Bálint", 100, 30, 15, 1));
+        }
+
+        private static int getNextFighter(int actFighter, int step)
+        {
+            actFighter = actualFighter + step;
+            if (actFighter >= fighters.Count)
+            {
+                actFighter = 0;
+            }
+
+            actualFighter = actFighter;
+            return actFighter;
         }
     }
 
